21c2de76b3aabef6c76c84f6f42909d1b5531beb
jahdev93 <jahdev93@gmail.com>
2024-09-09 15:19:28 +0200

    testing things out

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      blue: "#4b4dbd",
+      grey: "#5e5f6e",
+      black: "#0a0a0a",
+      red: "#bd3353",
+    },
+    fontFamily: {
+      ubuntumo: ["Ubuntu Mono", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
